e1aaff5b9f1480334ca1a62368ffb8445d888ee5
JaimeArturoMaganaNerio <neriojaime9@gmail.com>
2026-06-14 23:50:33 -0600

    Feat: agregando OptionEntity y Option

--- app/src/main/java/com/pdmcourse2026/basictemplate/data/database/entities/OptionEntity.kt
+++ app/src/main/java/com/pdmcourse2026/basictemplate/data/database/entities/OptionEntity.kt
@@ -1,2 +1,31 @@
 package com.pdmcourse2026.basictemplate.data.database.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.pdmcourse2026.basictemplate.data.model.Option
+
+
+@Entity(tableName = "options")
+data class OptionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val imageUrl: String,
+)
+
+// Mappers: convertir entre Entity (de la BD) y modelo (de la app)
+fun OptionEntity.toModel(): Option {
+    return Option(
+        id = id,
+        name = name,
+        imageUrl = imageUrl,
+    )
+}
+
+fun Option.toEntity(): OptionEntity {
+    return OptionEntity(
+        id = id,
+        name = name,
+        imageUrl = imageUrl,
+    )
+}
--- app/src/main/java/com/pdmcourse2026/basictemplate/data/model/Option.kt
+++ app/src/main/java/com/pdmcourse2026/basictemplate/data/model/Option.kt
@@ -1,2 +1,8 @@
 package com.pdmcourse2026.basictemplate.data.model
 
+
+data class Option(
+    val id: Int = 0,
+    val name: String,
+    val imageUrl: String,
+)
